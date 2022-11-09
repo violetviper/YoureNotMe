190e2c152776f9acd0fcdb0cf34a4ac720fcd094
davidbabazadeh <21daveab12@gmail.com>
2022-11-08 22:10:57 -0800

    seperate server.js classes using module.exports and require

--- public/client.js
+++ public/client.js
@@ -1,12 +1,17 @@
 const joinBtn = document.getElementById("join-btn");
 const hostBtn = document.getElementById("host-btn");
 const nickname = document.getElementById("username-input");
+const htmlPlayerList = document.getElementById("player-list");
+
+nickname.value = localStorage.getItem('nickname') ? JSON.parse(localStorage.getItem('nickname')) : '';
 
 joinBtn.onclick = joinRoomClicked;
 hostBtn.onclick = createRoomClicked;
 
 let playerType;
 let playerIcon;
+let game;
+let gamestate;
 
 // function setVisibility(id, isVisible) {
 //   if (isVisible) document.getElementById(id).style.visibility = "visible";
@@ -17,16 +22,19 @@ function selectIcon() {
 
 }
 
-function setPage(pageId) {
+function setPage(pageID) {
+  gamestate = pageID;
   const list = document.querySelectorAll('.page');
   for (let i = 0; i < list.length - 1; i++) {
     list[i].style.visibility = "hidden";
   }
-  document.getElementById(pageId).style.visibility = "visible";
+  document.getElementById(pageID).style.visibility = "visible";
 }
 
 function init() {
   setPage("client-lobby");
+  playertype = null;
+
 }
 
 init();
@@ -38,16 +46,27 @@ let socket;
 function joinRoomClicked() {
   if (/\S/.test(nickname.value)) {
     playerType = "member";
-    // socket.emit('join', {username:nickname.value});
-    //
-    //
-    //
+    localStorage.setItem('nickname', JSON.stringify(nickname.value));
+    setPage("loading");
+
     console.log("Request connect");
     socket = io.connect("http://localhost:3000"); // change to server with rooms
     socket.on('connected', () => {
-      console.log("connected, now requesting join room");
+      socketConnected();
       const roomID = document.getElementById("room-code-input").value; // TODO: CHANGE LATER
+      console.log("requesting join room " + roomID);
       socket.emit("joinRoom", {"roomID" : roomID, "nickname" : nickname.value}); // todo: data add pfp and room code
+
+      socket.on("successfullyJoinedRoom", data => {
+        setPage("room-lobby");
+        game = data["game"];
+        console.log("Congrats, you've joined room " + game.roomID + "! :)")
+        gameUpdate();
+      });
+      socket.on("roomNotFound", data => {
+        console.log("Room ID " + roomID + " does not exist )):");
+        setPage("client-lobby");
+      })
     });
     // todo: regulate times joined to one; hide html join form
   }
@@ -56,26 +75,58 @@ function joinRoomClicked() {
 function createRoomClicked() {
   if (/\S/.test(nickname.value)) {
     playerType = "host";
+    setPage("loading");
+
     console.log("Request connect");
     socket = io.connect("http://localhost:3000"); // change to server with rooms
 
     socket.on('connected', () => {
-      console.log("connected, now requesting host room");
+      socketConnected();
+      console.log("Requesting to host room");
       socket.emit("hostRoom", {"nickname" : nickname.value}); // todo: data add pfp and room code
-    });
 
+      socket.on("roomCreated", data => {
+        setPage("room-lobby");
+        game = data["game"];
+        console.log("Congrats, you're hosting room " + game.roomID + "! :D");
+        gameUpdate();
+      });
+
+    });
 
   }
 }
 
-//
-// socket.on('playerJoined', playerDict => {
-//   console.log(playerDict);
-//   for([key, val] of Object.entries(playerDict)) {
-//     const user = document.createElement("li");
-//     user.innerHTML = val.name;
-//     // TODO: add profile pic
-//     user.id = key;
-//     document.getElementById("player-list").appendChild(user);
-//   }
-// });
+function gameUpdate() {
+  document.getElementById("join-link-text").innerHTML = "RoomID: " + game.roomID;
+  if (gamestate == "room-lobby") {
+    playerListUpdate();
+  }
+}
+
+function playerListUpdate() {
+  htmlPlayerList.innerHTML = "";
+  for(const [nickname, playerObj] of Object.entries(game.playerMap)) {
+    const userHTML = document.createElement("li");
+    userHTML.innerHTML = nickname;
+    if (playerObj.status == "host") {
+      userHTML.innerHTML += "👑";
+    }
+    htmlPlayerList.appendChild(userHTML);
+
+  }
+}
+
+function socketConnected() {
+  console.log("connected to server");
+  socket.on("disconnect",() => {
+    socket.disconnect();
+
+    console.log("Disconnected from server.");
+    init();
+  });
+  socket.on("newPlayerJoined", data => {
+    game = data["game"];
+    playerListUpdate();
+  });
+}
---
+++ src/Game.js
@@ -0,0 +1,38 @@
+const Player = require("./Player");
+// TODO: separate classes into different files.
+class Game {
+  constructor(roomID) {
+    this.hostName = "";
+    this.roomID = roomID;
+    this.playerMap = {};
+    this.playerCount = 0;
+    this.gamestate = "INITIAL";
+    this.pointGoal = 0;
+    this.hotseatPlayer = 0;
+    this.gamepack = "";
+  }
+
+  addPlayer = function(clientID, nickname) {
+    this.playerMap[nickname] = new Player(clientID, nickname);
+    this.playerCount++;
+  }
+
+  chooseHotseat = function() {
+    this.hotseatPlayer = Math.floor(Math.random() * playerCount);
+  }
+
+  removePlayer = function(nickname) {
+    this.playerMap.delete(nickname);
+    this.playerCount--;
+  }
+
+  setHost = function(nickname) {
+    if (this.hostName != "") {
+      this.playerMap[this.hostName].status = "member";
+    }
+    this.hostName = nickname;
+    this.playerMap[nickname].status = "host";
+  }
+
+}
+module.exports = Game;
